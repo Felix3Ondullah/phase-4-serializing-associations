d9c703e0c40e43ef6aa087a368beed66c1ee5c7e
Felix Ondullah <felixondullah@gmail.com>
2022-11-24 10:31:20 +0300

    Coded along

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -2,8 +2,8 @@ class DirectorsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
-    directors = Director.all
-    render json: directors
+    director = Director.find(params[:id])
+    render json: director, include: ['movies', 'movies.reviews']
   end
 
   def show
